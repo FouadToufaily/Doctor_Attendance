78d9e99e6f123b836e00c5794c0a12459ec9318b
larafakih <larafakih08@gmail.com>
2023-08-27 17:50:14 +0300

    login updated

--- Doctor_Attendance/Doctor_Attendance/Pages/S/Account/Login.cshtml.cs
+++ Doctor_Attendance/Doctor_Attendance/Pages/S/Account/Login.cshtml.cs
@@ -1,3 +1,4 @@
+using Doctor_Attendance.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -8,16 +9,15 @@ namespace Doctor_Attendance.Pages.S.Account
     public class LoginModel : PageModel
     {
 
-        private readonly UserManager<IdentityUser> userManager;
-        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly AppDBContext _context;
 
-        [Required]
-        [EmailAddress]
-        public string Email { get; set; }
-
-        [Required]
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
+        public LoginModel (AppDBContext context)
+        {
+            _context = context;
+        }
+        
+       [BindProperty]
+        public LoginInputModel Input { get; set; }
 
         [Display(Name = "Remember Me")]
         public bool RememberMe { get; set; }
@@ -26,6 +26,40 @@ namespace Doctor_Attendance.Pages.S.Account
         {
             return Page();
         }
-      
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            string LogInValid = "";
+            if (ModelState.IsValid)
+            {
+                if (_context.Users.Any(u => u.Username == Input.Username))//username exists
+                {
+                    //if password is incorrect
+                    if (_context.Users.Any(u => u.Username == Input.Username && !(u.Password == Input.Password)))
+
+                        LogInValid = "Incorrect Password";
+                    else
+                        return RedirectToPage("/S/Test/Index");
+
+                }//username is incorrect
+                else
+                     LogInValid = "This username does not exists";
+
+            }
+            return Page();
+        }
+        public class LoginInputModel
+        {
+            [Required]
+            [Display(Name = "Username")]
+            public string Username { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            
+        }
     }
 }
