d1abe8df5f6d72d8b8bc65c33b90edfc2d15b41d
larafakih <larafakih08@gmail.com>
2023-09-24 15:30:55 +0300

    dummyDataScript

--- Database/DoctorsAttendanceData.sql
+++ Database/DoctorsAttendanceData.sql
@@ -14,82 +14,82 @@ insert into DEPARTMENT values (null,'Sciences Infirmières',10), (null,'Laborator
 insert into DEPARTMENT values (null,'Law',10), (null,'Political Sciences',10)
 
 /* Doctors in dep 1, Mathematis */
-insert into Doctor values (1, 1, 'D0001', 'Hassan','Abbas', 30, 'habbas@ul.edu.lb', 'Beirut'),
-                          (1, 1, 'D0002', 'Ibtissam','Zeaiter', 30, 'zibtissambbas@ul.edu.lb', 'Beirut'),
-                          (1, 1, 'D0003', 'Hassan','Ibrahim', 30, 'Ihassan@ul.edu.lb', 'Beirut'),
-                          (1, 2, 'D0004', 'Zeinab','Salloum', 30, 'szeinab@ul.edu.lb', 'Beirut'),
-                          (1, 1, 'D0005', 'Wael','Youssef', 30, 'ywael@ul.edu.lb', 'Beirut'),
-                          (1, 1, 'D0006', 'Youssef','Ayyad', 30, 'ayoussef@ul.edu.lb', 'Beirut'),
+insert into Doctor values (1, 1, 'D1', 'Hassan','Abbas', 30, 'habbas@ul.edu.lb', 'Beirut'),
+                          (1, 1, 'D2', 'Ibtissam','Zeaiter', 30, 'zibtissambbas@ul.edu.lb', 'Beirut'),
+                          (1, 1, 'D3', 'Hassan','Ibrahim', 30, 'Ihassan@ul.edu.lb', 'Beirut'),
+                          (1, 2, 'D4', 'Zeinab','Salloum', 30, 'szeinab@ul.edu.lb', 'Beirut'),
+                          (1, 1, 'D5', 'Wael','Youssef', 30, 'ywael@ul.edu.lb', 'Beirut'),
+                          (1, 1, 'D6', 'Youssef','Ayyad', 30, 'ayoussef@ul.edu.lb', 'Beirut'),
 
 /* Doctors in dep 2, Computer Science and Statistics */
-                          (2, 1, 'D0007', 'Zeinab','Assaghir', 30, 'azeinab@ul.edu.lb', 'Beirut'),
-                          (2, 1, 'D0008', 'Ahmad','Faour', 30, 'fahmad@ul.edu.lb', 'Beirut'),
-                          (2, 2, 'D0009', 'Iqbal','Hussein', 30, 'hiqbal@ul.edu.lb', 'Beirut'),
-                          (2, 1, 'D0010', 'Antoun','Yaakoub', 30, 'yantoun@ul.edu.lb', 'Beirut'),
-                          (2, 1, 'D0011', 'Zein','Ibrahim', 30, 'izein@ul.edu.lb', 'Beirut'),
-                          (2, 3, 'D0012', 'Kamal','Beydoun', 30, 'bkamal@ul.edu.lb', 'Beirut'),
-                          (2, 1, 'D0013', 'Bassem','Haidar', 30, 'hbassem@ul.edu.lb', 'Beirut');
+                          (2, 1, 'D7', 'Zeinab','Assaghir', 30, 'azeinab@ul.edu.lb', 'Beirut'),
+                          (2, 1, 'D8', 'Ahmad','Faour', 30, 'fahmad@ul.edu.lb', 'Beirut'),
+                          (2, 2, 'D9', 'Iqbal','Hussein', 30, 'hiqbal@ul.edu.lb', 'Beirut'),
+                          (2, 1, 'D10', 'Antoun','Yaakoub', 30, 'yantoun@ul.edu.lb', 'Beirut'),
+                          (2, 1, 'D11', 'Zein','Ibrahim', 30, 'izein@ul.edu.lb', 'Beirut'),
+                          (2, 3, 'D12', 'Kamal','Beydoun', 30, 'bkamal@ul.edu.lb', 'Beirut'),
+                          (2, 1, 'D13', 'Bassem','Haidar', 30, 'hbassem@ul.edu.lb', 'Beirut');
 
 /* Doctors in dep 3, Physics and Electronics */
-insert into Doctor values (3, 1, 'D0014', 'Yasser','Mhanna', 30, 'yamoha@ul.edu.lb', 'Beirut');
-insert into Doctor values (3, 1, 'D0015', 'Oussama','Bazzi', 30, 'boussama@ul.edu.lb', 'Beirut');
-insert into Doctor values (3, 2, 'D0016', 'Hassan','Ghamlouch', 30, 'ghassan@ul.edu.lb', 'Beirut');
-insert into Doctor values (3, 1, 'D0017', 'Rafik','Kattan', 30, 'krafik@ul.edu.lb', 'Beirut');
-insert into Doctor values (3, 3, 'D0018', 'Salah','Hamieh', 30, 'hsalah@ul.edu.lb', 'Beirut');
-insert into Doctor values (3, 1, 'D0019', 'Mouenes','Fadlallah', 30, 'fmouenes@ul.edu.lb', 'Beirut');
-insert into Doctor values (3, 1, 'D0020', 'Wissam','Mouzannar', 30, 'mwissam@ul.edu.lb', 'Beirut');
+insert into Doctor values (3, 1, 'D14', 'Yasser','Mhanna', 30, 'yamoha@ul.edu.lb', 'Beirut');
+insert into Doctor values (3, 1, 'D15', 'Oussama','Bazzi', 30, 'boussama@ul.edu.lb', 'Beirut');
+insert into Doctor values (3, 2, 'D16', 'Hassan','Ghamlouch', 30, 'ghassan@ul.edu.lb', 'Beirut');
+insert into Doctor values (3, 1, 'D17', 'Rafik','Kattan', 30, 'krafik@ul.edu.lb', 'Beirut');
+insert into Doctor values (3, 3, 'D18', 'Salah','Hamieh', 30, 'hsalah@ul.edu.lb', 'Beirut');
+insert into Doctor values (3, 1, 'D19', 'Mouenes','Fadlallah', 30, 'fmouenes@ul.edu.lb', 'Beirut');
+insert into Doctor values (3, 1, 'D20', 'Wissam','Mouzannar', 30, 'mwissam@ul.edu.lb', 'Beirut');
 
 /* Doctors in dep 4, Life and Earth Sciences */
-insert into Doctor values (4, 1, 'D0021', 'Mohamad','Mortada', 30, 'mortadamh@ul.edu.lb', 'Beirut');
-insert into Doctor values (4, 1, 'D0022', 'Ziad','Abdelrazzak', 30, 'aziad@ul.edu.lb', 'Beirut');
-insert into Doctor values (4, 2, 'D0023', 'Ali','Chaker', 30, 'chali@ul.edu.lb', 'Beirut');
-insert into Doctor values (4, 3, 'D0024', 'Kassem','Hamze', 30, 'khamze@ul.edu.lb', 'Beirut');
-insert into Doctor values (4, 2, 'D0025', 'Mahmoud','Homsi', 30, 'hmahmoud@ul.edu.lb', 'Beirut');
-insert into Doctor values (4, 1, 'D0026', 'Nabil','Amacha', 30, 'anabil@ul.edu.lb', 'Beirut');
-insert into Doctor values (4, 1, 'D0027', 'Haidar','Akl', 30, 'ahaidar@ul.edu.lb', 'Beirut');
+insert into Doctor values (4, 1, 'D21', 'Mohamad','Mortada', 30, 'mortadamh@ul.edu.lb', 'Beirut');
+insert into Doctor values (4, 1, 'D22', 'Ziad','Abdelrazzak', 30, 'aziad@ul.edu.lb', 'Beirut');
+insert into Doctor values (4, 2, 'D23', 'Ali','Chaker', 30, 'chali@ul.edu.lb', 'Beirut');
+insert into Doctor values (4, 3, 'D24', 'Kassem','Hamze', 30, 'khamze@ul.edu.lb', 'Beirut');
+insert into Doctor values (4, 2, 'D25', 'Mahmoud','Homsi', 30, 'hmahmoud@ul.edu.lb', 'Beirut');
+insert into Doctor values (4, 1, 'D26', 'Nabil','Amacha', 30, 'anabil@ul.edu.lb', 'Beirut');
+insert into Doctor values (4, 1, 'D27', 'Haidar','Akl', 30, 'ahaidar@ul.edu.lb', 'Beirut');
 
 /* Doctors in dep 5, Chemistry and Biochemistry */
-insert into Doctor values (5, 2, 'D0028', 'ghassan','ibrahim', 30, 'gibrahim@ul.edu.lb', 'Beirut');
-insert into Doctor values (5, 1, 'D0029', 'Rabih','Hussein', 30, 'hrabih@ul.edu.lb', 'Beirut');
-insert into Doctor values (5, 1, 'D0030', 'Kamal','Hariri', 30, 'hkamal@ul.edu.lb', 'Beirut');
-insert into Doctor values (5, 2, 'D0031', 'Leila','Ghannam', 30, 'gleila@ul.edu.lb', 'Beirut');
-insert into Doctor values (5, 1, 'D0032', 'Mazen','Kurdi', 30, 'kmazen@ul.edu.lb', 'Beirut');
-insert into Doctor values (5, 3, 'D0033', 'Mostapha','Hamieh', 30, 'hmostapha@ul.edu.lb', 'Beirut');
-insert into Doctor values (5, 1, 'D0034', 'Rami','Alakoum', 30, 'arami@ul.edu.lb', 'Beirut');
-insert into Doctor values (5, 1, 'D0035', 'Ali','Kanj', 30, 'kali@ul.edu.lb', 'Beirut'),
+insert into Doctor values (5, 2, 'D28', 'ghassan','ibrahim', 30, 'gibrahim@ul.edu.lb', 'Beirut');
+insert into Doctor values (5, 1, 'D29', 'Rabih','Hussein', 30, 'hrabih@ul.edu.lb', 'Beirut');
+insert into Doctor values (5, 1, 'D30', 'Kamal','Hariri', 30, 'hkamal@ul.edu.lb', 'Beirut');
+insert into Doctor values (5, 2, 'D31', 'Leila','Ghannam', 30, 'gleila@ul.edu.lb', 'Beirut');
+insert into Doctor values (5, 1, 'D32', 'Mazen','Kurdi', 30, 'kmazen@ul.edu.lb', 'Beirut');
+insert into Doctor values (5, 3, 'D33', 'Mostapha','Hamieh', 30, 'hmostapha@ul.edu.lb', 'Beirut');
+insert into Doctor values (5, 1, 'D34', 'Rami','Alakoum', 30, 'arami@ul.edu.lb', 'Beirut');
+insert into Doctor values (5, 1, 'D35', 'Ali','Kanj', 30, 'kali@ul.edu.lb', 'Beirut'),
 
 /* Doctors in dep 6 => 9,in public health faculty */
                          
-                         (6, 1, 'D0036', 'Tris', 'Limpricht', 45, 'tlimpricht1@gnu.org', 'Ladysmith'),
-                         (6, 1, 'D0037', 'Hermione', 'Dyster', 33, 'hdyster2@berkeley.edu', 'Boundiali'),
-						 (6, 2, 'D0038', 'Kesley', 'Leighfield', 34, 'kleighfielda@pen.io', 'Mujur Satu'),
-                         (6, 2, 'D0039', 'Dukie', 'Kainz', 37, 'dkainzb@domainmarket.com', 'Baota'),
+                         (6, 1, 'D36', 'Tris', 'Limpricht', 45, 'tlimpricht1@gnu.org', 'Ladysmith'),
+                         (6, 1, 'D37', 'Hermione', 'Dyster', 33, 'hdyster2@berkeley.edu', 'Boundiali'),
+						 (6, 2, 'D38', 'Kesley', 'Leighfield', 34, 'kleighfielda@pen.io', 'Mujur Satu'),
+                         (6, 2, 'D39', 'Dukie', 'Kainz', 37, 'dkainzb@domainmarket.com', 'Baota'),
 
-                         (7, 2, 'D0040', 'Sal', 'Upsale', 38, 'supsale3@mapy.cz', 'Pawak'),
-                         (7, 1, 'D0041', 'Geoff', 'Hatter', 41, 'ghatter4@ehow.com', 'Fornelos'),
-                         (7, 2, 'D0042', 'Alejandra', 'Shouler', 44, 'ashouler5@cbc.ca', 'Shuigou'),
-						 (7, 1, 'D0043', 'Nealy', 'MacKintosh', 46, 'nmackintoshc@ucoz.com', 'Eksjö'),
+                         (7, 2, 'D40', 'Sal', 'Upsale', 38, 'supsale3@mapy.cz', 'Pawak'),
+                         (7, 1, 'D41', 'Geoff', 'Hatter', 41, 'ghatter4@ehow.com', 'Fornelos'),
+                         (7, 2, 'D42', 'Alejandra', 'Shouler', 44, 'ashouler5@cbc.ca', 'Shuigou'),
+						 (7, 1, 'D43', 'Nealy', 'MacKintosh', 46, 'nmackintoshc@ucoz.com', 'Eksjö'),
 
-                         (8, 1, 'D0044', 'Dmitri', 'Agastina', 32, 'dagastina6@businessinsider.com', 'Quitilipi'),
-                         (8, 1, 'D0045', 'Finlay', 'Lipsett', 47, 'flipsett7@who.int', 'Kanuma'),
-                         (8, 1, 'D0046', 'Sibby', 'Dendle', 47, 'sdendle8@tumblr.com', 'Cassanayan'),
-						 (8, 2, 'D0047', 'Meghann', 'Conti', 35, 'mcontid@t.co', 'Ljungsbro'),
+                         (8, 1, 'D44', 'Dmitri', 'Agastina', 32, 'dagastina6@businessinsider.com', 'Quitilipi'),
+                         (8, 1, 'D45', 'Finlay', 'Lipsett', 47, 'flipsett7@who.int', 'Kanuma'),
+                         (8, 1, 'D46', 'Sibby', 'Dendle', 47, 'sdendle8@tumblr.com', 'Cassanayan'),
+						 (8, 2, 'D47', 'Meghann', 'Conti', 35, 'mcontid@t.co', 'Ljungsbro'),
 
-                         (9, 2, 'D0048', 'Randell', 'Eary', 49, 'reary9@blogger.com', 'Daciyao'),
-						 (9, 1, 'D0049', 'Reube', 'Tesimon', 30, 'rtesimon0@cnbc.com', 'Rzeczenica'),
-						 (9, 1, 'D0050', 'Lelia', 'Gillani', 39, 'lgillanie@naver.com', 'Krajan'),
-                         (9, 1, 'D0051', 'Dominga', 'Paddington', 48, 'dpaddingtonf@sourceforge.net', 'Moscow'),
+                         (9, 2, 'D48', 'Randell', 'Eary', 49, 'reary9@blogger.com', 'Daciyao'),
+						 (9, 1, 'D49', 'Reube', 'Tesimon', 30, 'rtesimon0@cnbc.com', 'Rzeczenica'),
+						 (9, 1, 'D50', 'Lelia', 'Gillani', 39, 'lgillanie@naver.com', 'Krajan'),
+                         (9, 1, 'D51', 'Dominga', 'Paddington', 48, 'dpaddingtonf@sourceforge.net', 'Moscow'),
 
 /* Doctors in dep 6 => 9,in Law faculty */
-                        (10, 1, 'D0052', 'Verina', 'Keywood', 41, 'vkeywoodg@harvard.edu', 'Saraza'),
-                        (10, 2, 'D0053', 'Tiebout', 'Stairs', 38, 'tstairsh@newsvine.com', 'Serh'),
-                        (10, 1, 'D0054', 'Nanice', 'Berens', 46, 'nberensi@cocolog-nifty.com', 'Russkaya Polyana'),
-                        (10, 1, 'D0055', 'Erminia', 'Giacubo', 39, 'egiacuboj@goo.gl', 'Taokeng'),
+                        (10, 1, 'D52', 'Verina', 'Keywood', 41, 'vkeywoodg@harvard.edu', 'Saraza'),
+                        (10, 2, 'D53', 'Tiebout', 'Stairs', 38, 'tstairsh@newsvine.com', 'Serh'),
+                        (10, 1, 'D54', 'Nanice', 'Berens', 46, 'nberensi@cocolog-nifty.com', 'Russkaya Polyana'),
+                        (10, 1, 'D55', 'Erminia', 'Giacubo', 39, 'egiacuboj@goo.gl', 'Taokeng'),
 
-                        (11, 1, 'D0056', 'Kirby', 'Weepers', 44, 'kweepersk@ibm.com', 'Jocotán'),
-                        (11, 2, 'D0057', 'Gilberta', 'Vaux', 44, 'gvauxl@usatoday.com', 'Charleroi'),
-                        (11, 2, 'D0058', 'Raquela', 'Littlewood', 49, 'rlittlewoodm@seattletimes.com', 'Hujiaba'),
-                        (11, 2, 'D0059', 'Butch', 'Layman', 37, 'blaymann@csmonitor.com', 'Xibing');
+                        (11, 1, 'D56', 'Kirby', 'Weepers', 44, 'kweepersk@ibm.com', 'Jocotán'),
+                        (11, 2, 'D57', 'Gilberta', 'Vaux', 44, 'gvauxl@usatoday.com', 'Charleroi'),
+                        (11, 2, 'D58', 'Raquela', 'Littlewood', 49, 'rlittlewoodm@seattletimes.com', 'Hujiaba'),
+                        (11, 2, 'D59', 'Butch', 'Layman', 37, 'blaymann@csmonitor.com', 'Xibing');
 
 /*HOS Doctor for Public Health Faculty */
 
